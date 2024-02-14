07d8cd4115b607831edf33db5210617807c4ddd6
MdShuvo18 <mdks33881@gmail.com>
2024-02-14 17:57:30 +0600

    get alphabets and keyboard interaction completed

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,7 +1,40 @@
-function playNow() {
-    const homeScreen = document.querySelector('#home-section');
-    homeScreen.classList.add('hidden');
+// function playNow() {
+//     const homeScreen = document.querySelector('#home-section');
+//     homeScreen.classList.add('hidden');
 
-    const playGroundSection = document.querySelector('#play-ground')
-    playGroundSection.classList.remove('hidden');
+//     const playGroundSection = document.querySelector('#play-ground')
+//     playGroundSection.classList.remove('hidden');
+
+//     getRandomAlphabet();
+// }
+
+
+// function getRandomAlphabet() {
+//     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+//     const alphabets = alphabetString.split('');
+//     console.log(alphabets);
+
+//     const randomNumber = Math.random() * 25;
+//     const index = Math.round(randomNumber);
+//     console.log(index);
+// }
+
+function continueGame() {
+    // generate a random alphabet
+    const alphabet = getRandomAlphabet();
+    console.log('your random alphabet number :', alphabet);
+
+    // set random generated alphabet
+    const foundRandomAlphabet = document.getElementById('current-alphabet');
+    foundRandomAlphabet.innerText = alphabet.toUpperCase();
+
+    setBackgrounColorById(alphabet);
+
+}
+
+
+function playNow() {
+    hideElementById('home-section');
+    showElementById('play-ground');
+    continueGame();
 }
---
+++ scripts/utility.js
@@ -0,0 +1,31 @@
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden')
+}
+
+function showElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden')
+}
+
+function getRandomAlphabet() {
+    // get or create an alphabet array
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    // console.log(alphabets);
+
+    // get random index between 0-25
+    const randomNumber = Math.random() * 25;
+    const index = Math.round(randomNumber);
+    // console.log(index);
+
+    const alphabet = alphabets[index];
+    // console.log(index, alphabet)
+    return alphabet;
+
+}
+
+function setBackgrounColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-amber-500')
+}
